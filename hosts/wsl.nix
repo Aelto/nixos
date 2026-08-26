0f4c4c744984cfa3d8aaf87278dc50f73152337c
Aelto <Aelto@users.noreply.github.com>
2026-08-26 17:34:10 +0200

    host: wsl: add auto optimise

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -17,4 +17,14 @@
   users.users.nixos.extraGroups = [ "podman" ];
   time.timeZone = "Europe/Paris";
   system.stateVersion = "26.05";
+
+  # System-wide Nix settings for this host
+  nix.settings.auto-optimise-store = true;
+
+  # Pair it with automatic weekly garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 }
